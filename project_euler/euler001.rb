616b4a621bac6ccc7f7eb9e8c99e084dd9fc1487
sufinsha <sufinsha@qburst.com>
2015-05-12 16:57:39 +0530

    # Solution for https://www.hackerrank.com/contests/projecteuler/challenges/euler001

--- project_euler/euler001.rb
+++ project_euler/euler001.rb
@@ -1,17 +1,23 @@
 # https://www.hackerrank.com/contests/projecteuler/challenges/euler001
 # Project Euler #1: Multiples of 3 and 5
 #
-def sum_of_multiples_of_3_or_5(num)
-  sum = 0
-  3.upto(num).select do |x|
-    sum += x if x % 3 == 0 || x % 5 == 0
-  end
-  sum
+#
+def sum_of_multiples(num, multiple)
+  # largest multiple of a number 'y' below 'x' is: x - x % y
+  upper = num - (num % multiple)
+  # an = a + d(n-1) => n = (an - a)/d + 1
+  count = (upper - multiple)/multiple + 1
+  # Sum of first 'n' numbers of an arithmetic progression is:
+  # s = n/2 * (2*a + (n-1)*d)
+  count * (2 * multiple + (count - 1) * multiple)/2
 end
 
 args = ARGF.readlines
 
 args[1..-1].each do |arg|
   num = arg.to_i - 1
-  p sum_of_multiples_of_3_or_5(num)
+  # Multiples of 3 or 5 = sum of multiples(3) +
+  #                       sum of multiples(5) -
+  #                       sum of multiples(15)
+  p sum_of_multiples(num, 3) + sum_of_multiples(num, 5) - sum_of_multiples(num, 15)
 end
